5dc3b603b6a0f3195d4e612c0740868770e74ebd
David Ohlemacher <ohlemacher@us.ibm.com>
2018-02-25 20:46:48 -0500

    Joy-8: Add adxl345 as turbo toggle control.
    Need to figure out mount and select the proper axis to use.
    
    DCO 1.1 Signed-off-by: David Ohlemacher <ohlemacher@us.ibm.com>

--- brians-joy.ino
+++ brians-joy.ino
@@ -10,9 +10,10 @@
 //------------------------------------------------------------
 
 #include "Joystick.h"
+#include <SparkFun_ADXL345.h>         // SparkFun ADXL345 Library
 
 // Slow down loop(). Be verbose. For debugging only.
-const bool verbose        = false;
+const bool verbose        = true;
 
 // Voltage mappings
 const float minOut = 0;
@@ -65,6 +66,8 @@ Joystick_ Joystick(
         hasAccelerator,
         hasBrake,
         hasSteering);
+
+ADXL345 adxl = ADXL345();             // USE FOR I2C COMMUNICATION
 
 void setButton0() {
     buttonState0 = ! digitalRead(button0);
@@ -213,6 +216,83 @@ void setOtherAxis() {
     }
 }
 
+void setTurboMode() {
+    // Accelerometer Readings
+    int x,y,z;
+    adxl.readAccel(&x, &y, &z); // Read the accelerometer values and store them in variables declared above x,y,z
+
+    int isTurboOn = 0;
+
+	if (x >= 20) {
+	    isTurboOn = 1;
+	}
+	Joystick.setButton(1, isTurboOn);
+
+    if (verbose) {
+		Serial.print("Turbo: ");
+		Serial.print(isTurboOn);
+		Serial.print(" [x=");
+		Serial.print(x);
+		Serial.print(" y=");
+		Serial.print(y);
+		Serial.print(" z=");
+		Serial.print(z);
+		Serial.println("]");
+	}
+}
+
+void setup_adxl() {
+
+  Serial.begin(9600);                 // Start the serial terminal
+  Serial.println("SparkFun ADXL345 Accelerometer Hook Up Guide Example");
+  Serial.println();
+
+  adxl.powerOn();                     // Power on the ADXL345
+
+  adxl.setRangeSetting(16);           // Give the range settings
+                                      // Accepted values are 2g, 4g, 8g or 16g
+                                      // Higher Values = Wider Measurement Range
+                                      // Lower Values = Greater Sensitivity
+
+  adxl.setSpiBit(0);                  // Configure the device to be in 4 wire SPI mode when set to '0' or 3 wire SPI mode when set to 1
+                                      // Default: Set to 1
+                                      // SPI pins on the ATMega328: 11, 12 and 13 as reference in SPI Library
+
+  adxl.setActivityXYZ(1, 0, 0);       // Set to activate movement detection in the axes "adxl.setActivityXYZ(X, Y, Z);" (1 == ON, 0 == OFF)
+  adxl.setActivityThreshold(75);      // 62.5mg per increment   // Set activity   // Inactivity thresholds (0-255)
+
+  adxl.setInactivityXYZ(1, 0, 0);     // Set to detect inactivity in all the axes "adxl.setInactivityXYZ(X, Y, Z);" (1 == ON, 0 == OFF)
+  adxl.setInactivityThreshold(75);    // 62.5mg per increment   // Set inactivity // Inactivity thresholds (0-255)
+  adxl.setTimeInactivity(10);         // How many seconds of no activity is inactive?
+
+  adxl.setTapDetectionOnXYZ(0, 0, 1); // Detect taps in the directions turned ON "adxl.setTapDetectionOnX(X, Y, Z);" (1 == ON, 0 == OFF)
+
+  // Set values for what is considered a TAP and what is a DOUBLE TAP (0-255)
+  adxl.setTapThreshold(50);           // 62.5 mg per increment
+  adxl.setTapDuration(15);            // 625 μs per increment
+  adxl.setDoubleTapLatency(80);       // 1.25 ms per increment
+  adxl.setDoubleTapWindow(200);       // 1.25 ms per increment
+
+  // Set values for what is considered FREE FALL (0-255)
+  adxl.setFreeFallThreshold(7);       // (5 - 9) recommended - 62.5mg per increment
+  adxl.setFreeFallDuration(30);       // (20 - 70) recommended - 5ms per increment
+
+  // Setting all interupts to take place on INT1 pin
+  //adxl.setImportantInterruptMapping(1, 1, 1, 1, 1);     // Sets "adxl.setEveryInterruptMapping(single tap, double tap, free fall, activity, inactivity);"
+                                                        // Accepts only 1 or 2 values for pins INT1 and INT2. This chooses the pin on the ADXL345 to use for Interrupts.
+                                                        // This library may have a problem using INT2 pin. Default to INT1 pin.
+
+  // Turn on Interrupts for each mode (1 == ON, 0 == OFF)
+  adxl.InactivityINT(1);
+  adxl.ActivityINT(1);
+  adxl.FreeFallINT(1);
+  adxl.doubleTapINT(1);
+  adxl.singleTapINT(1);
+
+//attachInterrupt(digitalPinToInterrupt(interruptPin), ADXL_ISR, RISING);   // Attach Interrupt
+
+}
+
 void setup() {
     if (verbose) {
         Serial.begin(9600);
@@ -261,7 +341,10 @@ void setup() {
     if (hasSteering) {
         Joystick.setSteeringRange(minAxis, maxAxis);
     }
+
     Joystick.begin();
+
+    setup_adxl();
 }
 
 void loop() {
@@ -271,7 +354,7 @@ void loop() {
     }
 
     setButton0();
-    setButton1();
+    setTurboMode();
     setXAxis();
     setOtherAxis();
 }
